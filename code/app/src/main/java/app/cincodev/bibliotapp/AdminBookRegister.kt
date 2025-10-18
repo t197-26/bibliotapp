2cfce5d4b6dc975082e8faecb3e4a4ebc3ea2ef9
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2025-10-18 16:50:38 -0300

    adicionado popup de confirmação de registro de material (admin)

--- code/app/src/main/java/app/cincodev/bibliotapp/AdminBookRegister.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/AdminBookRegister.kt
@@ -1,22 +1,25 @@
 package app.cincodev.bibliotapp
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class AdminBookRegister : AppCompatActivity() {
-    lateinit var capa:ImageView
+    lateinit var capa: ImageView
     lateinit var selecionarCapa: ImageButton
+    lateinit var cadastrarButton: Button
+    lateinit var descartarButton: Button
 
-    // Inside your Activity or Fragment
     private val pickImageLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             uri?.let {
-                // Here you get the selected image URI
-                capa.setImageURI(it) // Example: set it to an ImageView
+                capa.setImageURI(it)
             }
         }
 
@@ -26,14 +29,47 @@ class AdminBookRegister : AppCompatActivity() {
 
         capa = findViewById(R.id.capa)
         selecionarCapa = findViewById(R.id.selecionarCapa)
+        cadastrarButton = findViewById(R.id.cadastrarButton)
+        descartarButton = findViewById(R.id.descartarButton)
 
-    }
-
-    override fun onStart() {
-        super.onStart()
         selecionarCapa.setOnClickListener {
             pickImageLauncher.launch("image/*")
         }
+
+        cadastrarButton.setOnClickListener {
+            mostrarDialogoConfirmacao()
+        }
+    }
+
+    private fun mostrarDialogoConfirmacao() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_confirmation_book_register, null)
+
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        val btnCancelar = dialogView.findViewById<Button>(R.id.btnCancelar)
+        val btnConfirmar = dialogView.findViewById<Button>(R.id.btnConfirmar)
+
+        btnCancelar.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        btnConfirmar.setOnClickListener {
+            registrarLivro()
+            dialog.dismiss()
+
+            var intencao = Intent(this, AdminHome::class.java)
+            startActivity(intencao)
+        }
+
+        dialog.show()
+    }
+
+
+    private fun registrarLivro() {
+        // Aqui vai o código para salvar o livro
+        // Exemplo:
+        // Toast.makeText(this, "Livro cadastrado!", Toast.LENGTH_SHORT).show()
     }
 }
-
