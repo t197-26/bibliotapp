78866fd9a56a53bfa2725f7ce00c1216e79609a6
LRC <lucas.coelho@edu.unifor.br>
2025-10-19 19:47:04 -0300

    Adicionada tela de forget password

--- code/app/src/main/java/app/cincodev/bibliotapp/ForgetPassword.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/ForgetPassword.kt
@@ -1,20 +1,31 @@
 package app.cincodev.bibliotapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ForgetPassword : AppCompatActivity() {
+    lateinit var arrowBackButtonView: ImageButton
+    lateinit var sendButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_forget_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        arrowBackButtonView = findViewById(R.id.ForgetPasswordArrowBack)
+        arrowBackButtonView.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        sendButton = findViewById(R.id.ForgetPasswordSendButton)
+        sendButton.setOnClickListener {
+            Toast.makeText(applicationContext, "E-mail de recuperação enviado.", Toast.LENGTH_LONG).show()
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
