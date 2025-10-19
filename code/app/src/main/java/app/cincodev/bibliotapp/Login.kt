e7f077340567c364b425ca096cfda377626c206d
LRC <lucas.coelho@edu.unifor.br>
2025-10-19 19:16:42 -0300

    Adicionada tela de login

--- code/app/src/main/java/app/cincodev/bibliotapp/Login.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/Login.kt
@@ -1,20 +1,35 @@
 package app.cincodev.bibliotapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+    lateinit var arrowBackButtonView: ImageButton
+    lateinit var AccessButton: Button
+    lateinit var ForgetPasswordButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        arrowBackButtonView = findViewById(R.id.LoginArrowBack)
+        arrowBackButtonView.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        AccessButton = findViewById(R.id.LoginAccessButton)
+        AccessButton.setOnClickListener {
+            startActivity(Intent(this, UserHome::class.java))
+        }
+
+        AccessButton = findViewById(R.id.LoginForgetPasswordButton)
+        AccessButton.setOnClickListener {
+            startActivity(Intent(this, ForgetPassword::class.java))
         }
     }
 }
