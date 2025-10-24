6941670c1b797e3e140e195d4374dc84e1d7ad86
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2025-10-24 10:48:02 -0300

    Adicionada tela de home do usuário

--- code/app/src/main/java/app/cincodev/bibliotapp/UserHome.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/UserHome.kt
@@ -1,20 +1,54 @@
 package app.cincodev.bibliotapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 class UserHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val dataset = arrayOf(
+            QuickBook(R.drawable.book_01, "Gerenciamento de serviços de TI na prática", "2 dias restantes"),
+            QuickBook(R.drawable.book_02, "Governança de TI", "15 dias restantes"),
+            QuickBook(R.drawable.book_03, "Sistemas de Informação", "2 dias atrasado")
+        )
+
+        val dataset_2 = arrayOf(
+            QuickPlace(R.drawable.ic_quickplace, "Sala q12", "24/09/2025 11:00\n24/09/2025 11:00"),
+            QuickPlace(R.drawable.book_01, "Gerencimento de TI", "Disponível"),
+            QuickPlace(R.drawable.book_03, "Sistemas de Informação", "3 dias")
+        )
+
+        val quickBookAdapter = QuickBookAdapter(dataset)
+
+        val recyclerView: RecyclerView = findViewById(R.id.quickBookRecycler)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = quickBookAdapter
+
+        val quickPlaceAdapter = QuickPlaceAdapter(dataset_2)
+
+        val recyclerView2: RecyclerView = findViewById(R.id.quickPlaceRecycler)
+        recyclerView2.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView2.adapter = quickPlaceAdapter
+
+        val btnDigitalizacoes: ImageView = findViewById(R.id.btnDIgitalizacoes)
+        val btnReservarEspaco: ImageView = findViewById(R.id.btnReservarEspaco)
+
+        btnDigitalizacoes.setOnClickListener { openDigitalizacoes() }
+        btnReservarEspaco.setOnClickListener { openReservarEspaco() }
+    }
+
+    private fun openDigitalizacoes() {
+        val intent = Intent(this, UserDigitalizations::class.java)
+        startActivity(intent)
+    }
+
+    private fun openReservarEspaco() {
+        val intent = Intent(this, UserSpaceBooking::class.java)
+        startActivity(intent)
     }
 }
