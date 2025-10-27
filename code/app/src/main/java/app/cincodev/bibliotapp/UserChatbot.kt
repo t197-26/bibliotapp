aaacb7e8301439e7d9f108f481b0aa55710bb558
Pedro Henrique <pedrocaseis@hotmail.com>
2025-10-27 12:07:48 -0300

    feature/tela-user-chatbot

--- code/app/src/main/java/app/cincodev/bibliotapp/UserChatbot.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/UserChatbot.kt
@@ -1,20 +1,23 @@
 package app.cincodev.bibliotapp
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class UserChatbot : AppCompatActivity() {
+
+    private lateinit var arrowBackButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user_chatbot)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        arrowBackButton = findViewById(R.id.UserChatbotArrowBack)
+        arrowBackButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed() // volta para a tela anterior
         }
     }
 }
