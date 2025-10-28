92a5b6ff948359217a1b268e1d9fee8ab3077532
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2025-10-28 14:26:55 -0300

    Adicionada activity da tela de edição de exemplar

--- code/app/src/main/java/app/cincodev/bibliotapp/AdminBookEditor.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/AdminBookEditor.kt
@@ -1,20 +1,31 @@
 package app.cincodev.bibliotapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class AdminBookEditor : AppCompatActivity() {
 
     lateinit var arrowBackButtonView: ImageButton
+    lateinit var editarExemplar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_book_editor)
 
         arrowBackButtonView = findViewById(R.id.adminEditBookArrowBack)
+        editarExemplar = findViewById(R.id.editarExemplar)
+
         arrowBackButtonView.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        editarExemplar.setOnClickListener {
+            startActivity(Intent(this, AdminEditExemplar::class.java))
+
+        }
+
     }
 }
