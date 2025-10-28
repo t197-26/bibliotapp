04e1597e420e620889eab722c41a7676ebf24540
LRC <lucas.coelho@edu.unifor.br>
2025-10-27 21:25:55 -0300

    Corrigido posicionamento de variáveis na AdminHome

--- code/app/src/main/java/app/cincodev/bibliotapp/AdminHome.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/AdminHome.kt
@@ -3,20 +3,19 @@ package app.cincodev.bibliotapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-lateinit var arrowBackButtonView : ImageButton
-lateinit var DigitazacoesConstraintLayout : ConstraintLayout
-lateinit var CadastrarMaterialConstraintLayout : ConstraintLayout
-lateinit var EditarMaterialConstraintLayout : ConstraintLayout
-lateinit var EmprestimosConstraintLayout : ConstraintLayout
-lateinit var ReservasEspacoConstraintLayout : ConstraintLayout
+
 
 class AdminHome : AppCompatActivity() {
+    lateinit var arrowBackButtonView : ImageButton
+    lateinit var DigitazacoesConstraintLayout : ConstraintLayout
+    lateinit var CadastrarMaterialConstraintLayout : ConstraintLayout
+    lateinit var EditarMaterialConstraintLayout : ConstraintLayout
+    lateinit var EmprestimosConstraintLayout : ConstraintLayout
+    lateinit var ReservasEspacoConstraintLayout : ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
