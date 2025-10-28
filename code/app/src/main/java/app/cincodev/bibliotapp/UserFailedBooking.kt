5890d8b08764667f082cc2e1666cc4fc4d3ee80d
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2025-10-28 18:06:08 -0300

    Correção na tela de chatbot e adicionado intents para as telas de confirmação, erro e processamento de reserva

--- code/app/src/main/java/app/cincodev/bibliotapp/UserFailedBooking.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/UserFailedBooking.kt
@@ -1,20 +1,22 @@
 package app.cincodev.bibliotapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class UserFailedBooking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user_failed_booking)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        lifecycleScope.launch {
+            delay(2000) // 2 seconds
+            val intent = Intent(this@UserFailedBooking, UserSuccesfulBooking::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- code/app/src/main/java/app/cincodev/bibliotapp/UserSuccesfulBooking.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/UserSuccesfulBooking.kt
@@ -1,20 +1,21 @@
 package app.cincodev.bibliotapp
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class UserSuccesfulBooking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user_succesful_booking)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        lifecycleScope.launch {
+            delay(2000) // 2 seconds
+            val intent = Intent(this@UserSuccesfulBooking, UserBookDetail::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
