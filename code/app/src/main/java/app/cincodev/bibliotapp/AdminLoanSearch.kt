2c6a55b99b4a9335e3ff8c64441938e7c5b9d08e
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2025-10-28 00:51:40 -0300

    Correção ortográfica e inclusão do botão Voltar funcional

--- code/app/src/main/java/app/cincodev/bibliotapp/AdminLoanSearch.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/AdminLoanSearch.kt
@@ -1,14 +1,23 @@
 package app.cincodev.bibliotapp
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class AdminLoanSearch : AppCompatActivity() {
+
+    lateinit var arrowBackButtonView: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_loan_search)
+
+        arrowBackButtonView = findViewById(R.id.adminLoanSearchArrowBack)
+        arrowBackButtonView.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
 
         val dataset = arrayOf(
             Loan("2357911", "245099", "0 dias", "Em dia"),
