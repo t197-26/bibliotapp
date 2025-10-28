fe5e7c6fc1aed22070c4644eb97b0e7390fd8d50
LRC <lucas.coelho@edu.unifor.br>
2025-10-28 18:09:28 -0300

    Hotfix: Corrigida tela de confirmação congelada

--- code/app/src/main/java/app/cincodev/bibliotapp/UserProcessingBooking.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/UserProcessingBooking.kt
@@ -1,20 +1,24 @@
 package app.cincodev.bibliotapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class UserProcessingBooking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user_processing_booking)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        lifecycleScope.launch {
+            delay(1500)
+            val intent = Intent(this@UserProcessingBooking, UserSuccesfulBooking::class.java)
+            startActivity(intent)
         }
     }
 }
