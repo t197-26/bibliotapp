1e02282f63f9d6ec4fb35601140cc18b992e532e
LRC <lucas.coelho@edu.unifor.br>
2025-11-04 20:38:01 -0300

    Adicionada criação de usuário minima

--- code/app/src/main/java/app/cincodev/bibliotapp/AccountRegister.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/AccountRegister.kt
@@ -2,67 +2,82 @@ package app.cincodev.bibliotapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+// Exemplo de uso
+//        fb1.collection(
+//            "users"
+//        ).document("mNyLNe3BduZqYRDjjzhw").delete()
+
+//        fb1.collection(
+//            "users"
+//        ).document("mNyLNe3BduZqYRDjjzhw").update(mapOf("name" to "Pedro"))
+
+//        fb1.collection("users").document().get().addOnSuccessListener{
+//            result ->
+//            etNome.setText(result.get("name").toString())
+//        }
+
 
 class AccountRegister : AppCompatActivity() {
-    lateinit var arrowBackButtonView: ImageButton
-    lateinit var registerButton: Button
-    lateinit var missingFieldsTextView: TextView
-    lateinit var MatriculaEditText: EditText
-    lateinit var FullNameEditText: EditText
-    lateinit var EmailEditText: EditText
-    lateinit var PasswordEditText: EditText
-    lateinit var PasswordConfirmationEditText: EditText
-    lateinit var passwordNotMatchingTextView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_register)
 
-        arrowBackButtonView = findViewById(R.id.AccountRegisterArrowBack)
+        val matriculaEditText               = findViewById<EditText>(R.id.AccountRegisterMatriculaEditText)
+        val fullNameEditText                = findViewById<EditText>(R.id.AccountRegisterFullNameEditText)
+        val emailEditText                   = findViewById<EditText>(R.id.AccountRegisterEmailEditText)
+        val passwordEditText                = findViewById<EditText>(R.id.AccountRegisterPasswordEditText)
+        val passwordConfirmationEditText    = findViewById<EditText>(R.id.AccountRegisterPasswordConfirmationEditText)
+        val missingFieldsTextView           = findViewById<TextView>(R.id.AccountRegisterMissingFieldsTextView)
+        val passwordNotMatchingTextView     = findViewById<TextView>(R.id.AccountRegisterPasswordNotMatchingTextView)
+        val arrowBackButtonView             = findViewById<ImageButton>(R.id.AccountRegisterArrowBack)
+        val registerButton                  = findViewById<Button>(R.id.AccountRegisterRegisterButton)
+
         arrowBackButtonView.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-        MatriculaEditText = findViewById(R.id.AccountRegisterMatriculaEditText)
-        FullNameEditText = findViewById(R.id.AccountRegisterFullNameEditText)
-        EmailEditText = findViewById(R.id.AccountRegisterEmailEditText)
-        PasswordEditText = findViewById(R.id.AccountRegisterPasswordEditText)
-        PasswordConfirmationEditText = findViewById(R.id.AccountRegisterPasswordConfirmationEditText)
-        missingFieldsTextView = findViewById(R.id.AccountRegisterMissingFieldsTextView)
-        passwordNotMatchingTextView = findViewById(R.id.AccountRegisterPasswordNotMatchingTextView)
-        registerButton = findViewById(R.id.AccountRegisterRegisterButton)
-
+        val db = Firebase.firestore
         registerButton.setOnClickListener {
-
             missingFieldsTextView.visibility = View.INVISIBLE
-
             passwordNotMatchingTextView.visibility = View.INVISIBLE
-
-            if (MatriculaEditText.text.length == 0
-                || FullNameEditText.text.length == 0
-                || EmailEditText.text.length == 0
-                || PasswordEditText.text.length == 0
-                || PasswordConfirmationEditText.text.length == 0) {
+            if (
+                matriculaEditText.text.toString().isEmpty()
+                || fullNameEditText.text.toString().isEmpty()
+                || emailEditText.text.toString().isEmpty()
+                || passwordEditText.text.toString().isEmpty()
+                || passwordConfirmationEditText.text.toString().isEmpty()) {
 
                 missingFieldsTextView.visibility = View.VISIBLE
 
-            } else if (PasswordEditText.text.toString() != PasswordConfirmationEditText.text.toString()) {
+            } else if (passwordEditText.text.toString() != passwordConfirmationEditText.text.toString()) {
 
                 passwordNotMatchingTextView.visibility = View.VISIBLE
 
             } else {
+                lifecycleScope.launch {
+                    userWrite(
+                        db = db,
+                        id = matriculaEditText.text.toString(),
+                        name = fullNameEditText.text.toString(),
+                        email = emailEditText.text.toString(),
+                        password = passwordEditText.text.toString()
+                    )
+                }
+
                 val dialogView = layoutInflater.inflate(R.layout.dialog_confirmation_user_register, null)
 
                 val dialog = AlertDialog.Builder(this)
@@ -79,5 +94,29 @@ class AccountRegister : AppCompatActivity() {
                 dialog.show()
             }
         }
+    }
+
+    suspend fun userWrite(
+        db : FirebaseFirestore,
+        id : String,
+        name : String,
+        email : String,
+        password : String
+    ) {
+        val data = mapOf(
+            "name" to name,
+            "email" to email,
+            "password" to password
+        )
+
+        db.collection("users").document(id)
+            .set(data)
+            .addOnSuccessListener {
+                println("Documento salvo com ID: $id")
+            }
+            .addOnFailureListener { e ->
+                println("Erro ao salvar documento: ${e.message}")
+            }
+            .await()
     }
 }
