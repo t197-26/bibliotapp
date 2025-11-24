31dd49020f903c7e2816b3e9bd6ad09f967428bf
Italo Almeida <italoa.dev@gmail.com>
2025-11-24 13:48:42 -0300

    feat: finish spaces fetch from Firestore implementation

--- code/app/src/main/java/app/cincodev/bibliotapp/ReserveSpace.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/ReserveSpace.kt
@@ -5,29 +5,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.slider.RangeSlider
+import java.util.Calendar
+import java.util.Locale
 
 
 class ReserveSpace : AppCompatActivity() {
+    lateinit var botaoCalendario: ImageButton
+    lateinit var rangeSlider: RangeSlider
+    lateinit var tvSelectedRange: TextView
     lateinit var arrowBackButtonView: ImageButton
+    lateinit var tvDate: TextView
+
+    lateinit var goToSelectionButton: Button
+    private var selectedDate: String? = null
+    private var startHour: Int = 8
+    private var endHour: Int = 9
+    private val calendar: Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reserve_space)
 
-        val dataset = arrayOf(
-            ReservaItem("Cubículo C10", "Entrada: 20/09/2025 09:00", "Saída: 20/09/2025 11:00"),
-            ReservaItem("Cubículo C11", "Entrada: 20/09/2025 11:00", "Saída: 20/09/2025 13:00"),
-            ReservaItem("Cubículo C12", "Entrada: 20/09/2025 15:00", "Saída: 20/09/2025 16:00"),
-            ReservaItem("Cubículo C10", "Entrada: 20/09/2025 09:00", "Saída: 20/09/2025 11:00"),
-            ReservaItem("Cubículo C11", "Entrada: 20/09/2025 11:00", "Saída: 20/09/2025 13:00"),
-            ReservaItem("Cubículo C12", "Entrada: 20/09/2025 15:00", "Saída: 20/09/2025 13:00"),
-            ReservaItem("Cubículo C12", "Entrada: 20/09/2025 15:00", "Saída: 20/09/2025 13:00"),
-            ReservaItem("Cubículo C12", "Entrada: 20/09/2025 15:00", "Saída: 20/09/2025 13:00"),
-            ReservaItem("Cubículo C12", "Entrada: 20/09/2025 15:00", "Saída: 20/09/2025 13:00")
-        )
+        val dataset = arrayOf<ReservaItem>()
 
         val customAdapter = ListaReservasEspacoAdapter(this, dataset)
 
@@ -37,20 +41,69 @@ class ReserveSpace : AppCompatActivity() {
     }
 
     override fun onStart() {
-        val botaoCalendario = findViewById<ImageButton>(R.id.botaoCalendario)
+        botaoCalendario = findViewById(R.id.botaoCalendario)
+        rangeSlider = findViewById(R.id.reserveTimeSlider)
+        tvSelectedRange = findViewById(R.id.tvSelectedRange)
+        tvDate = findViewById(R.id.reserveDate)
+        goToSelectionButton = findViewById(R.id.goToSelection)
+
+        tvDate.text = String.format(Locale.US, "%02d/%02d/%04d", calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR))
+
+        rangeSlider.values = listOf(startHour.toFloat(), endHour.toFloat())
+        onRangeSliderValuesChange(rangeSlider)
+
+        rangeSlider.addOnChangeListener { slider, _, _ ->
+            onRangeSliderValuesChange(slider)
+        }
 
         botaoCalendario.setOnClickListener {
-            val datePickerDialog: DatePickerDialog = DatePickerDialog(this)
-
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, year, month, dayOfMonth ->
+                    selectedDate = String.format(Locale.US, "%02d/%02d/%04d", dayOfMonth, month + 1, year)
+                    tvDate.text = selectedDate
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
             datePickerDialog.show()
         }
 
-        val goToSelectionButton = findViewById<Button>(R.id.goToSelection)
-
         goToSelectionButton.setOnClickListener {
-            startActivity(Intent(this, SelectSpace::class.java))
+            val intent = Intent(this, SelectSpace::class.java)
+
+            intent.putExtra("selected_date", tvDate.text)
+            intent.putExtra("start_hour", startHour.toString())
+            intent.putExtra("end_hour", endHour.toString())
+
+            startActivity(intent)
         }
 
         super.onStart()
+    }
+
+    private fun onRangeSliderValuesChange(slider: RangeSlider) {
+        val step = slider.stepSize
+        val min = slider.valueFrom
+        val max = slider.valueTo
+
+        startHour = slider.values[0].toInt()
+        endHour = slider.values[1].toInt()
+
+        if (startHour >= endHour) {
+            val tryEnd = (startHour + step).coerceAtMost(max)
+
+            if (tryEnd > endHour) {
+                endHour = tryEnd.toInt()
+            } else {
+                startHour = (endHour - step).coerceAtLeast(min).toInt()
+            }
+
+            slider.values = listOf(startHour.toFloat(), endHour.toFloat())
+            return
+        }
+
+        tvSelectedRange.text = getString(R.string.horario_reserva, startHour, endHour)
     }
 }
--- code/app/src/main/java/app/cincodev/bibliotapp/SelectSpace.kt
+++ code/app/src/main/java/app/cincodev/bibliotapp/SelectSpace.kt
@@ -1,17 +1,26 @@
 package app.cincodev.bibliotapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.GridLayout
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SelectSpace : AppCompatActivity() {
     lateinit var arrowBackButtonView: ImageButton
+    lateinit var salasGridLayout: GridLayout
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +37,84 @@ class SelectSpace : AppCompatActivity() {
         btnReservar.setOnClickListener {
             showConfirmationDialog()
         }
+
+
+        val tvSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
+        val tvStartHour = findViewById<TextView>(R.id.tvStartHour)
+        val tvEndHour = findViewById<TextView>(R.id.tvEndHour)
+
+        val selectedDate = intent.getStringExtra("selected_date")
+        val startHour = intent.getStringExtra("start_hour")
+        val endHour = intent.getStringExtra("end_hour")
+
+        tvSelectedDate.text = selectedDate
+
+        if (startHour != null) {
+            tvStartHour.text = formatHour(startHour)
+        }
+        if (endHour != null) {
+            tvEndHour.text = formatHour(endHour)
+        }
     }
 
     override fun onStart() {
+        salasGridLayout = findViewById(R.id.gridSalas)
+
+        db.collection("espacos")
+            .document("biblioteca")
+            .get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+
+                    val salas = document.get("salas") as? List<String>
+                    salas?.let {
+                        preencheGrid(it)
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e("FIRESTORE", "Erro: ", e)
+            }
+
         arrowBackButtonView = findViewById(R.id.LoginArrowBack)
         arrowBackButtonView.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
         super.onStart()
+    }
+
+    private fun preencheGrid(salas: List<String>) {
+        salasGridLayout.removeAllViews()
+
+        salas.forEachIndexed { index, sala ->
+            val ocupada = index % 2 == 0
+
+            val button = Button(this, null).apply {
+                text = sala
+
+                background = ContextCompat.getDrawable(this@SelectSpace, if (ocupada) R.drawable.tag_background_yellow else R.drawable.bg_rounded_button)
+
+                layoutParams = GridLayout.LayoutParams().apply {
+                    width = 0
+                    height = ViewGroup.LayoutParams.WRAP_CONTENT
+                    columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+                }
+
+                setOnClickListener {
+                }
+            }
+
+            salasGridLayout.addView(button)
+        }
+    }
+
+    private fun formatHour(stringHour: String): String {
+        val hour = stringHour.toIntOrNull()
+        if (hour == null) return "00:00"
+        val amPm = if (hour < 12) "AM" else "PM"
+        val h = if (hour == 0) 12 else if (hour > 12) hour - 12 else hour
+        return String.format("%02d:00 %s", h, amPm)
     }
 
     private fun showConfirmationDialog() {
